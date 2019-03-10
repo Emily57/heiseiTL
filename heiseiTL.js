b48458d35dc8ca671d5ce58c11003b9bcdc9d547
Emily <42111747+misaki0915@users.noreply.github.com>
2019-03-10 22:24:38 +0900

    Create base

---
+++ heiseiTL.js
@@ -0,0 +1,74 @@
+//問題と解答
+qa = new Array();
+qa[0] = ["ベルリンの壁崩壊",19891110,1989,11,10];
+qa[1] = ["ゲームボーイ発売",19890421,1989,4,21];
+qa[2] = ["Wii発売",20061119,2006,11,19];
+qa[3] = ["PS3発売",20061111,2006,11,11];
+qa[4] = ["たまごっち発売",19961123,1996,11,23];
+qa[5] = ["消費税3%導入",19890401,1989,4,1];
+qa[6] = ["mixi(SNS)開始",20040303,2004,3,3];
+qa[7] = ["Windows Vista 全世界に一般リリース",20070130,2007,1,30];
+qa[8] = ["LINEサービス開始",20110623,2011,6,23];
+qa[9] = ["女々しくて発売",20091021,2009,10,21];
+
+
+//初期設定
+count = 0; //問題番号
+setReady();
+
+//初期設定
+function setReady() {
+	count = 0; //問題番号
+	answers = new Array(); //解答記録
+	quiz(); //最初の問題
+}
+
+//最初の問題
+quiz();
+
+//問題表示
+//問題表示
+function quiz() {
+	var s, n, r;
+  var before = "before";
+  var after = "after";
+  r = 1; //ゆくゆくはランダムにしたい
+	//問題
+	document.getElementById("text_q").innerHTML = (count + 1) + "問目：" + qa[count][0] + "は、" + qa[count+r][0] +"より？";
+	//選択肢
+	s = "";
+	s += "【<a href='javascript:answer(0)'>前</a>】";
+	s += "【<a href='javascript:answer(1)'>後</a>】";
+	document.getElementById("text_s").innerHTML = s;
+}
+
+//解答表示
+function answer(num) {
+	var s, r;
+  r = 1;
+	s = (count + 1) + "問目：" + qa[count][0] + "は、" + qa[count+r][0] +"より？";;
+	//答え合わせ
+  if(qa[count][1]<qa[count+r][1] && num === 0){
+    answers[count] = "<p>【正解】</p>" ;
+  } else if (qa[count][1]>qa[count+r][1] && num === 1){
+    answers[count] = "<p>【正解】</p>";
+  } else {
+    answers[count] = "<p>【不正解】</p>"
+  }
+
+  answers[count] += "<p>" + qa[count][0] + "は、" + qa[count][2] + "年" + qa[count][3] + "月" +qa[count][4] + "日</p>";
+  answers[count] += "<p>" + qa[count+r][0] + "は、" + qa[count+r][2] + "年" + qa[count+r][3] + "月" +qa[count+r][4] + "日</p>";
+
+	s += answers[count];
+	document.getElementById("text_a").innerHTML = s;
+
+	//次の問題を表示
+	count++;
+	if (count < qa.length) {
+		quiz();
+	} else {
+		//終了
+		document.getElementById("text_q").innerHTML = "";
+		document.getElementById("text_s").innerHTML = "";
+	}
+}
